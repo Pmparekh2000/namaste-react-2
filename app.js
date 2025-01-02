900cce37588543c4f648406c616908afdefa0b3c
Pmparekh2000 <pmp.tech2000@gmail.com>
2025-01-02 10:55:48 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,37 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement(
+      "h1",
+      { id: "h1-tag" },
+      "Hello from h1 tag from child1"
+    ),
+    React.createElement(
+      "h2",
+      { id: "h2-tag" },
+      "Hello from h1 tag from child1"
+    ),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement(
+      "h1",
+      { id: "h1-tag" },
+      "Hello from h1 tag from child2"
+    ),
+    React.createElement(
+      "h2",
+      { id: "h2-tag" },
+      "Hello from h1 tag from child2"
+    ),
+  ]),
+]);
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading", name: "hi" },
+  "Hello World from React JS 123"
+);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("header"));
+root.render(parent);
