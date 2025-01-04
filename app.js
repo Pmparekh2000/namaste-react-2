258bdecbfcac3e1b72259c1ef285eb2e109d5df8
Pmparekh2000 <pmp.tech2000@gmail.com>
2025-01-04 06:38:12 +0530

    Learnt about React components

--- app.js
+++ app.js
@@ -1,40 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement(
-      "h1",
-      { id: "h1-tag" },
-      "Hello from h1 tag from child1"
-    ),
-    React.createElement(
-      "h2",
-      { id: "h2-tag" },
-      "Hello from h1 tag from child1"
-    ),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement(
-      "h1",
-      { id: "h1-tag" },
-      "Hello from h1 tag from child2"
-    ),
-    React.createElement(
-      "h2",
-      { id: "h2-tag" },
-      "Hello from h1 tag from child2"
-    ),
-  ]),
-]);
-
 const heading = React.createElement(
   "h1",
   { id: "heading", name: "hi" },
   "Hello World from React JS 123"
 );
 
-console.log(parent);
+const number = 1000;
 
-const root = ReactDOM.createRoot(document.getElementById("header"));
-root.render(parent);
+const Title = () => {
+  return <div id="heading">Title 🚀</div>;
+};
+
+const HeadingComponent = (props) => {
+  console.log("Getting props as", props);
+
+  return (
+    <div>
+      {Title()}
+      <Title />
+      <Title></Title>
+      <h1>HeadingComponent</h1>
+    </div>
+  );
+};
+
+// console.log("jsxHeading is", jsxHeading);
+// console.log("heading is", heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
