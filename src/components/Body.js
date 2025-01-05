9db86c743944a899924bcfe5d3ad5453cd0f4568
Pmparekh2000 <pmp.tech2000@gmail.com>
2025-01-05 19:09:25 +0530

    Consuming data from Swiggy API and filtering based on ratings

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,28 +1,91 @@
 import RestaurantCard from "./RestaurantCard";
-import { restaurants } from "../util/dummyRestaurantData";
+import { useEffect, useState } from "react";
 
 const Body = () => {
+  const [restaurants, setRestuarants] = useState(null);
+  const [displayRestaurants, setDisplayRestaurants] = useState(null);
+  const [filteredRestaurants, setFilteredRestaurants] = useState(null);
+
+  const getRestaurantData = async () => {
+    const restaurantsReadableStream = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.981607&lng=77.6958882&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const restaurantsData = await restaurantsReadableStream.json();
+    setRestuarants(
+      restaurantsData.data.cards[1].card.card.gridElements.infoWithStyle
+        .restaurants
+    );
+  };
+
+  useEffect(() => {
+    getRestaurantData();
+  }, []);
+
+  useEffect(() => {
+    const restaurantData = restaurants?.map((restaurant) => {
+      const restaurantInfo = restaurant.info;
+      return {
+        id: restaurantInfo.id,
+        name: restaurantInfo.name,
+        areaName: restaurantInfo.areaName,
+        cuisines: restaurantInfo.cuisines,
+        eta: restaurantInfo.sla.slaString,
+        avgRating: restaurantInfo.avgRating,
+        cloudinaryImageId: restaurantInfo.cloudinaryImageId,
+        costForTwo: restaurantInfo.costForTwo,
+      };
+    });
+    setDisplayRestaurants(restaurantData);
+    setFilteredRestaurants(restaurantData);
+  }, [restaurants]);
+
+  const getTopRatedRestuarants = () => {
+    const topRatedRestaurants = displayRestaurants.filter(
+      (restaurant) => restaurant.avgRating > 4.5
+    );
+    setFilteredRestaurants(topRatedRestaurants);
+  };
+
+  const removeAllFilters = () => {
+    setFilteredRestaurants(displayRestaurants);
+  };
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        {filteredRestaurants ? (
+          <button
+            className="filter-button"
+            onClick={() => getTopRatedRestuarants()}
+          >
+            Top Rated Restaurants
+          </button>
+        ) : (
+          <></>
+        )}
+      </div>
+      <div>
+        <button onClick={() => removeAllFilters()}>Remove Filter</button>
+      </div>
       <div className="res-container">
-        {restaurants.map((restaurant, index) => {
-          return (
-            <RestaurantCard
-              key={index}
-              resname={restaurant.data?.resname || "Punjab Bistro"}
-              cuisine={
-                restaurant.data?.cuisine.join(", ") ||
-                "North Indian, Paneer Starter"
-              }
-              ratings={restaurant.data?.ratings || 4.5 + " rating"}
-              eta={restaurant.data?.eta || 15}
-              photoId={
-                restaurant.data?.photoId || "c58b85509b8cc4440f9aa2252ca98e04"
-              }
-            />
-          );
-        })}
+        {filteredRestaurants ? (
+          filteredRestaurants.map((restaurant) => {
+            return (
+              <RestaurantCard
+                key={restaurant.id}
+                name={restaurant.name}
+                areaName={restaurant.areaName}
+                cuisines={restaurant.cuisines.join(", ")}
+                avgRating={restaurant.avgRating + " rating"}
+                eta={restaurant.eta}
+                cloudinaryImageId={restaurant.cloudinaryImageId}
+                costForTwo={restaurant.costForTwo}
+              />
+            );
+          })
+        ) : (
+          <h1>Loading</h1>
+        )}
       </div>
     </div>
   );
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,17 +1,31 @@
 import { IMAGE_CDN } from "../util/constants";
 
 const RestaurantCard = (props) => {
-  const { resname, cuisine, ratings, eta, photoId } = props;
+  const {
+    name,
+    areaName,
+    cuisines,
+    avgRating,
+    eta,
+    cloudinaryImageId,
+    costForTwo,
+  } = props;
   const styleCard = {
     backgroundColor: "#f0f0f0",
   };
   return (
     <div className="res-card" style={styleCard}>
-      <img className="res-logo" alt="res-logo" src={IMAGE_CDN + photoId}></img>
-      <h3>{resname}</h3>
-      <h4>{cuisine}</h4>
-      <h4>{ratings} stars</h4>
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src={IMAGE_CDN + cloudinaryImageId}
+      ></img>
+      <h3>{name}</h3>
+      <h4>{areaName}</h4>
+      <h4>{cuisines}</h4>
+      <h4>{avgRating} stars</h4>
       <h4>{eta} mins</h4>
+      <h4>{costForTwo}</h4>
     </div>
   );
 };
